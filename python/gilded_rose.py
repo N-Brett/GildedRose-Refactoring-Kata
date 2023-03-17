0971d0d7a82ab331c7f3d87d363b1c8cf1c45cc7
Natalie Brett <Natalie.Brett@os.uk>
2023-03-17 17:18:07 +0000

    Added tests, created classes for items, changed update_quality method

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,36 +5,21 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def order(name, sell_in, quality):
+        if name == "Aged Brie":
+            return AgedBrie(name, sell_in, quality)
+        if name == "Sulfuras, Hand of Ragnaros":
+            return Sulfuras(name, sell_in, quality)
+        if name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePasses(name, sell_in, quality)
+        # if name == "Conjured":
+        #     return Conjured(name, sell_in, quality) # TODO implement conjured class
+        else:
+            return Item(name, sell_in, quality)
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            item.day()
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -44,3 +29,37 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    def day(self):
+        self.sell_in -= 1 # should this be at the end?
+        if 50 > self.quality > 0:
+            if self.sell_in <= 0:
+                self.quality -= 2
+            else:
+                self.quality -= 1
+
+
+class AgedBrie(Item):
+    def day(self):
+        self.sell_in -= 1
+        if 50 > self.quality > 0:
+            self.quality += 1
+
+
+class Sulfuras(Item):
+    def day(self):
+        return
+
+
+class BackstagePasses(Item):
+    def day(self):
+        self.sell_in -= 1
+        if 50 > self.quality:
+            if self.sell_in <= 0:
+                self.quality == 0
+            elif 5 >= self.sell_in > 0:
+                self.quality += 3
+            elif 10 >= self.sell_in > 5:
+                self.quality += 2
+            else:
+                self.quality += 1
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,13 +4,93 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
+class GildedRoseNormalItemsTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+        item = [GildedRose.order("foo", 0, 0)]
+        gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", item[0].name)
 
-        
+    def test_normal_item_quality_before_sell_date(self):
+        item = [GildedRose.order("Bread", 10, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(9, item[0].quality)
+        self.assertEqual(9, item[0].sell_in)
+
+    def test_normal_item_quality_after_sell_date(self):
+        item = [GildedRose.order("Beer", 0, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(8, item[0].quality)
+
+    def test_quality_not_negative(self):
+        item = [GildedRose.order("Cheese", 0, 0)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(0, item[0].quality)
+    
+    def test_item_sell_in_decreased(self):
+        item = [GildedRose.order("Healing Potion", 10, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(9, item[0].sell_in)
+
+
+class GildedRoseSpecialItemsTest(unittest.TestCase):
+    def test_sulfuras(self):
+        item = [GildedRose.order("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(80, item[0].quality)
+
+    def test_aged_brie(self):
+        item = [GildedRose.order("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(11, item[0].quality)
+
+    # def test_aged_brie_after_sell_date(self):
+    #     item = [GildedRose.order("Aged Brie", 0, 10)]
+    #     gilded_rose = GildedRose(item)
+    #     gilded_rose.update_quality()
+    #     self.assertEqual(12, item[0].quality)
+
+    def test_backstage_pass_sell_in_ten_days(self):
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(12, item[0].quality)
+
+    def test_backstage_pass_sell_in_six_days(self):
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 6, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(12, item[0].quality)
+    
+    def test_backstage_pass_sell_in_five_days(self):
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(13, item[0].quality)
+
+    def test_backstage_pass_sell_in_one_day(self):
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 1, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(13, item[0].quality)
+
+    def test_backstage_pass_sell_in_zero(self):
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(0, item[0].quality)
+
+    # def test_conjured_items(self):
+    #     item = [Item("Conjured", 10, 10)]
+    #     gilded_rose = GildedRose(item)
+    #     gilded_rose.update_quality()
+    #     self.assertEqual(8, item[0].quality)
+
 if __name__ == '__main__':
     unittest.main()
