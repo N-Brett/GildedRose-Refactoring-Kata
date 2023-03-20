cf5bac16f6b2da842c3660186deb42a362b4c391
Natalie Brett <Natalie.Brett@os.uk>
2023-03-20 16:49:23 +0000

    flake8

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import GildedRose
 
 
 class GildedRoseNormalItemsTest(unittest.TestCase):
@@ -11,6 +11,12 @@ class GildedRoseNormalItemsTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", item[0].name)
 
+    def test_item_sell_in_decreased(self):
+        item = [GildedRose.order("Healing Potion", 10, 10)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(9, item[0].sell_in)
+
     def test_normal_item_quality_before_sell_date(self):
         item = [GildedRose.order("Bread", 10, 10)]
         gilded_rose = GildedRose(item)
@@ -19,69 +25,63 @@ class GildedRoseNormalItemsTest(unittest.TestCase):
         self.assertEqual(9, item[0].sell_in)
 
     def test_normal_item_quality_after_sell_date(self):
-        item = [GildedRose.order("Beer", 0, 10)]
+        item = [GildedRose.order("Ale", 0, 10)]
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(8, item[0].quality)
 
     def test_quality_not_negative(self):
-        item = [GildedRose.order("Cheese", 0, 0)]
+        item = [GildedRose.order("Cheddar", 0, 0)]
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(0, item[0].quality)
-    
-    def test_item_sell_in_decreased(self):
-        item = [GildedRose.order("Healing Potion", 10, 10)]
-        gilded_rose = GildedRose(item)
-        gilded_rose.update_quality()
-        self.assertEqual(9, item[0].sell_in)
 
 
 class GildedRoseSpecialItemsTest(unittest.TestCase):
-    def test_sulfuras(self):
-        item = [GildedRose.order("Sulfuras, Hand of Ragnaros", 0, 80)]
-        gilded_rose = GildedRose(item)
-        gilded_rose.update_quality()
-        self.assertEqual(80, item[0].quality)
-
     def test_aged_brie(self):
         item = [GildedRose.order("Aged Brie", 10, 10)]
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(11, item[0].quality)
 
+    def test_sulfuras(self):
+        item = [GildedRose.order("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(item)
+        gilded_rose.update_quality()
+        self.assertEqual(80, item[0].quality)
+
     def test_backstage_pass_sell_in_more_ten_days(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 15, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 15, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(11, item[0].quality)
 
     def test_backstage_pass_sell_in_ten_days(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 10, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(12, item[0].quality)
 
     def test_backstage_pass_sell_in_six_days(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 6, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 6, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(12, item[0].quality)
-    
+
     def test_backstage_pass_sell_in_five_days(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 5, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(13, item[0].quality)
 
     def test_backstage_pass_sell_in_one_day(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 1, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 1, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(13, item[0].quality)
 
     def test_backstage_pass_sell_in_zero(self):
-        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        item = [GildedRose.order("Backstage passes to a TAFKAL80ETC concert", 0, 10)]  # noqa E501
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(0, item[0].quality)
@@ -97,6 +97,7 @@ class GildedRoseSpecialItemsTest(unittest.TestCase):
         gilded_rose = GildedRose(item)
         gilded_rose.update_quality()
         self.assertEqual(6, item[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from gilded_rose import *
+from gilded_rose import GildedRose
 
 if __name__ == "__main__":
-    print ("OMGHAI!")
+    print("OMGHAI!")
     items = [
             GildedRose.order(name="+5 Dexterity Vest", sell_in=10, quality=20),
             GildedRose.order(name="Aged Brie", sell_in=2, quality=0),
-            GildedRose.order(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-            GildedRose.order(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-            GildedRose.order(name="Elixir of the Mongoose", sell_in=5, quality=7),
-            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-            GildedRose.order(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+            GildedRose.order(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),  # noqa E501
+            GildedRose.order(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),  # noqa E501
+            GildedRose.order(name="Elixir of the Mongoose", sell_in=5, quality=7),  # noqa E501
+            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),  # noqa E501
+            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),  # noqa E501
+            GildedRose.order(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),  # noqa E501
+            GildedRose.order(name="Conjured Mana Cake", sell_in=3, quality=6),  # noqa E501
             ]
 
     days = 2
